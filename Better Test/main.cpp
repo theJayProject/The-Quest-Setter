5444db5b985e0ef75758b2c363bb7d8af2496107
TheJayProject <joannawojtusiakit@gmail.com>
2024-10-07 14:46:06 +0200

    Tidied up code

--- Better Test/main.cpp
+++ Better Test/main.cpp
@@ -5,40 +5,22 @@ int main() {
     for (std::string Answer_ph; Answer_ph != "end";) {
         std::cout << "Would you like to add, read, complete or delete a task?\n";
         std::cin >> Answer_ph;
-        std::string taskName_ph;
         if (Answer_ph == "add") {
-            std::cout << "What is the task name?\n";
-            std::cin >> taskName_ph;
-            tasks.insert(std::make_pair(taskName_ph, false));
-            std::cout << "Added " << taskName_ph << " as incomplete.\n";
-            taskList();
+            addTask();
         }
         else if (Answer_ph == "read") {
             taskList();
         }
         else if (Answer_ph == "complete") {
-            bool complete = true;
-            std::cout << "Which task would you like to complete?\n";
-            std::cin >> taskName_ph;
-            tasks.erase(tasks.find(taskName_ph));
-            tasks.insert(std::make_pair(taskName_ph, true));
-            std::cout << taskName_ph << " is now complete.\n";
-            taskList();
+            completeTask();
         }
         else if (Answer_ph == "delete") {
-            std::string taskDelete_ph;
-            std::cout << "Which task would you like to delete?\n";
-            std::cin >> taskDelete_ph;
-            tasks.erase(tasks.find(taskDelete_ph));
-            std::cout << taskDelete_ph << " has been deleted successfully\n";
-            taskList();
+            deleteTask();
         }
         else {
             std::cout << "Error: command misunderstood.\n";
-            return 0;
         }
     }
-
         return 0;
 }
 
--- Better Test/main.h
+++ Better Test/main.h
@@ -4,6 +4,9 @@
 
 #define Tasks
 std::map<std::string, bool> tasks;
+std::string taskName_ph;
+
+//Listing Tasks
 #define TaskList    
 auto taskList(std::map<std::string, bool>::iterator it = tasks.begin()) {
     std::cout << "\n";
@@ -11,5 +14,43 @@ auto taskList(std::map<std::string, bool>::iterator it = tasks.begin()) {
         std::cout << "Task: " << it->first << ", Status: " << it->second << std::endl;
         ++it;
         std::cout << std::endl;
+    }
+}
+//Adding Tasks
+#define ADDTASK
+auto addTask() {
+
+    std::cout << "What is the task name?\n";
+    std::cin >> taskName_ph;
+    tasks.insert(std::make_pair(taskName_ph, false));
+    std::cout << "Added " << taskName_ph << " as incomplete.\n";
+}
+//Deleting Tasks
+#define DELETETASK
+auto deleteTask() {
+    std::cout << "Which task would you like to delete?\n";
+    std::cin >> taskName_ph;
+    if (tasks.find(taskName_ph) != tasks.end()) {
+        tasks.erase(tasks.find(taskName_ph));
+        std::cout << taskName_ph << " has been deleted successfully\nTasks remaining:\n";
+        taskList();
+    }
+    else {
+        std::cout << "Task not found. Please try again.\n";
+    }
+}
+//Completing Tasks
+#define COMPLETETASK
+auto completeTask() {
+    bool complete = true;
+    std::cout << "Which task would you like to complete?\n";
+    std::cin >> taskName_ph;
+    if (tasks.find(taskName_ph) != tasks.end()) {
+        tasks.erase(tasks.find(taskName_ph));
+        tasks.insert(std::make_pair(taskName_ph, complete));
+        std::cout << taskName_ph << " is now complete.\n";
+    }
+    else {
+        std::cout << "Task not found. Please try again.\n";
     }
 }
