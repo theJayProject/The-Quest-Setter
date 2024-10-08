984d92433d1cece22bb09ce381e7451becce81fb
TheJayProject <joannawojtusiakit@gmail.com>
2024-10-08 09:11:52 +0200

    Introduced SDL window

--- Better Test/main.cpp
+++ Better Test/main.cpp
@@ -1,26 +1,31 @@
 #include "main.h"
+#include "visual.h"
 
-int main() {
+
+
+int main(int argc, char** argv) {
+    init_SDL_window();
+    //Initiating and opening the window with SDL
     //Adding, reading, editing tasks introduced     
-    for (std::string Answer_ph; Answer_ph != "end";) {
-        std::cout << "Would you like to add, read, complete or delete a task?\n";
-        std::cin >> Answer_ph;
-        if (Answer_ph == "add") {
-            addTask();
-        }
-        else if (Answer_ph == "read") {
-            taskList();
-        }
-        else if (Answer_ph == "complete") {
-            completeTask();
-        }
-        else if (Answer_ph == "delete") {
-            deleteTask();
-        }
-        else {
-            std::cout << "Error: command misunderstood.\n";
-        }
-    }
+    //for (std::string Answer_ph; Answer_ph != "end";) {
+    //    std::cout << "Would you like to add, read, complete or delete a task?\n";
+    //    std::cin >> Answer_ph;
+    //    if (Answer_ph == "add") {
+    //        addTask();
+    //    }
+    //    else if (Answer_ph == "read") {
+    //        taskList();
+    //    }
+    //    else if (Answer_ph == "complete") {
+    //        completeTask();
+    //    }
+    //    else if (Answer_ph == "delete") {
+    //        deleteTask();
+    //    }
+    //    else {
+    //        std::cout << "Error: command misunderstood.\n";
+    //    }
+    //}
         return 0;
 }
 
--- Better Test/visual.h
+++ Better Test/visual.h
@@ -1,1 +1,51 @@
 #pragma once
+#include <SDL.h>
+#define INITIALISE_WINDOW
+//Initialising and rendering the window
+int init_SDL_window() {
+    //Error handling
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "Failed to initialize the SDL2 Library\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return -1;
+    }
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        680, 480,
+        0);
+    //Error handling
+    if (!window)
+    {
+        std::cout << "Failed to create window\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+    //Error handling
+    if (!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return -1;
+    }
+    //Updating events and quit button
+    bool keep_window_open = true;
+    while (keep_window_open)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) > 0)
+        {
+            switch (e.type)
+            {
+            case SDL_QUIT:
+                keep_window_open = false;
+                break;
+            }
+
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+    return 1;
+}
